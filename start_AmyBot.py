715dd18618405c5829df53ae978a4164ab2b943b
Alexandru Preda <alexandrupreda@zoho.com>
2017-06-30 18:26:31 +0100

    autocorrect was added modifications were made to hello.aiml

--- start_AmyBot.py
+++ start_AmyBot.py
@@ -1,14 +1,28 @@
 import aiml
 import os
+from autocorrect import spell
+
+def spellChecker(message):
+    new_message = ""
+    for word in message.split():
+        word = spell(word)
+        new_message = new_message + word + " "
+    return new_message
+
+def checkResponseForAction(message):
+    #Shutdown
+    if ("bye" in message):
+        exit()
+
 
 kernel = aiml.Kernel()
 
 kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
 
-kernel.setPredicate("amy_name", "AmyBot")
-kernel.setPredicate("amy_short","Amy")
-kernel.setPredicate("creator", "Alexandru Preda")
-kernel.setPredicate("location", "Uni of Essex, in Colchester")
+# kernel.setPredicate("amy_name", "AmyBot")
+# kernel.setPredicate("amy_short","Amy")
+# kernel.setPredicate("creator", "Alexandru Preda")
+# kernel.setPredicate("location", "Uni of Essex, in Colchester")
 
 #kernel.setPredicate("bot_car_brand", "Tesla")
 #kernel.setPredicate("bot_favorite_weather", "chilly")
@@ -17,9 +31,13 @@ kernel.setPredicate("location", "Uni of Essex, in Colchester")
 # kernel now ready for use
 while True:
     message = raw_input("User: ")
-    if message == "exit":
-        exit()
-    else:
-        bot_response = kernel.respond(message)
-        print "AmyBot: " + bot_response
-        # Do something with bot_response
+    new_message = spellChecker(message)
+
+    bot_response = kernel.respond(new_message)
+    print "AmyBot: " + bot_response
+    # Do something with bot_response
+    checkResponseForAction(bot_response.lower())
+
+
+
+
