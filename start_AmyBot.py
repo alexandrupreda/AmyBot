069a4af4ca5d9513444ebe7b299fe3355e321f66
Alexandru Preda <alexandrupreda@zoho.com>
2017-07-01 17:18:42 +0100

    constructing linear conversation for cars.aiml

--- start_AmyBot.py
+++ start_AmyBot.py
@@ -1,6 +1,10 @@
 import aiml
 import os
+import sys
+import time
 from autocorrect import spell
+
+console = "CONSOLE: "
 
 def spellChecker(message):
     new_message = ""
@@ -9,9 +13,20 @@ def spellChecker(message):
         new_message = new_message + word + " "
     return new_message
 
+def checkUserInputForAction(message):
+    return
+    # Restart
+    # if ("restart" in message):
+    #     print console + "AmyBot is restarting..."
+    #     time.sleep(2)
+    #     os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+
 def checkResponseForAction(message):
     #Shutdown
     if ("bye" in message):
+        print console + "AmyBot has shutdown"
         exit()
 
 
@@ -19,7 +34,7 @@ kernel = aiml.Kernel()
 
 kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
 
-# kernel.setPredicate("amy_name", "AmyBot")
+kernel.setPredicate("amy_name", "AmyBot")
 # kernel.setPredicate("amy_short","Amy")
 # kernel.setPredicate("creator", "Alexandru Preda")
 # kernel.setPredicate("location", "Uni of Essex, in Colchester")
@@ -29,11 +44,13 @@ kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
 
 
 # kernel now ready for use
+print console + "AmyBot is operational..."
 while True:
-    message = raw_input("User: ")
-    new_message = spellChecker(message)
+    user_message = raw_input("User: ")
+    checkUserInputForAction(user_message)
+    new_user_message = spellChecker(user_message)
 
-    bot_response = kernel.respond(new_message)
+    bot_response = kernel.respond(new_user_message)
     print "AmyBot: " + bot_response
     # Do something with bot_response
     checkResponseForAction(bot_response.lower())
